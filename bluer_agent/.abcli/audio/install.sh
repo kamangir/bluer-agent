1768e6c92dd8283f551dad225b2e0b4e41b8724f
kamangir <arash@kamangir.net>
2025-12-23 09:22:59 +0330

    ++

--- bluer_agent/.abcli/audio/install.sh
+++ bluer_agent/.abcli/audio/install.sh
@@ -3,10 +3,14 @@
 function bluer_agent_audio_install() {
     if [[ "$abcli_is_rpi" == true ]]; then
         sudo apt install -y sox libsox-fmt-all
-    elif [[ "$abcli_is_mac" == true ]]; then
-        brew install sox
-    else
-        bluer_ai_log_error "@agent: audio: install: do not know how to install."
-        return 1
+        return
     fi
+
+    if [[ "$abcli_is_mac" == true ]]; then
+        brew install sox
+        return
+    fi
+
+    bluer_ai_log_error "@agent: audio: install: do not know how to install."
+    return 1
 }
