caa11da91a49d6140a3bb4c3531336f72615ee1d
kamangir <arash@kamangir.net>
2025-12-23 11:19:31 +0330

    ++

--- bluer_agent/.abcli/tests/transcription.sh
+++ bluer_agent/.abcli/tests/transcription.sh
@@ -3,19 +3,37 @@
 function test_bluer_agent_transcribe() {
     local options=$1
 
+    local do_play=0
+    [[ "$abcli_is_github_workflow" == false ]] &&
+        do_play=1
+
+    if [[ "$abcli_is_github_workflow" == false ]]; then
+        bluer_ai_eval ,$options \
+            bluer_agent_transcribe \
+            filename=farsi.wav \
+            $object_name \
+            play=$do_play \
+            language=fa,verbose \
+            --length 10
+        [[ $? -ne 0 ]] && return 1
+
+        bluer_ai_hr
+    fi
+
     bluer_ai_eval ,$options \
         bluer_agent_transcribe \
         filename=farsi.wav \
         $BLUER_AGENT_TRANSCRIPTION_TEST_OBJECT \
-        download,~record,~play \
+        download,~record,play=$do_play \
         language=fa,verbose
     [[ $? -ne 0 ]] && return 1
+
     bluer_ai_hr
 
     bluer_ai_eval ,$options \
         bluer_agent_transcribe \
         filename=english.wav \
         $BLUER_AGENT_TRANSCRIPTION_TEST_OBJECT \
-        download,~record,~play \
+        download,~record,play=$do_play \
         language=en,verbose
 }
