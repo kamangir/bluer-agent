2fc3b15b94cba6b13c73fd6e3f9a20395619444c
kamangir <arash@kamangir.net>
2025-12-23 12:59:22 +0330

    pipeline fix, hopefully

--- bluer_agent/tests/test_audio_properties.py
+++ bluer_agent/tests/test_audio_properties.py
@@ -9,13 +9,9 @@ def test_audio_properties():
     assert isinstance(properties.as_str(), str)
 
     command = properties.record_command(filename="this/that.wav")
-    assert isinstance(command, str)
+    assert isinstance(command, list)
+    for item in command:
+        assert isinstance(item, str)
 
     parser = argparse.ArgumentParser("some name")
-    parser.add_argument(
-        "task",
-        type=str,
-        help="some-task",
-    )
     AudioProperties.add_args(parser)
-    args = parser.parse_args()
