f093a25acc2387a9f5946bfe970c72dfa9fd9ffc
kamangir <arash@kamangir.net>
2025-12-23 15:59:29 +0330

    ++

--- bluer_agent/.abcli/audio.sh
+++ bluer_agent/.abcli/audio.sh
@@ -14,8 +14,7 @@ function bluer_agent_audio() {
 
 if [[ "$abcli_is_rpi" == true ]]; then
     export BLUER_AGENT_AUDIO_ALSA_HW=$(python3 -m bluer_agent.audio.detect_alsa_capture_hw)
-
-    bluer_ai_log "ALSA capture device: $BLUER_AGENT_AUDIO_ALSA_HW"
+    bluer_ai_log "🎤 ALSA capture device: $BLUER_AGENT_AUDIO_ALSA_HW"
 fi
 
 bluer_ai_source_caller_suffix_path /audio
