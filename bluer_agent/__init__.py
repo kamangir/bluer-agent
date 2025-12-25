24332739b87ef186931a9049de02699d8061d0e4
kamangir <arash@kamangir.net>
2025-12-25 13:54:00 +0330

    ++

--- bluer_agent/__init__.py
+++ bluer_agent/__init__.py
@@ -4,7 +4,7 @@ ICON = "👾"
 
 DESCRIPTION = f"{ICON} an AI agent."
 
-VERSION = "5.125.1"
+VERSION = "5.126.1"
 
 REPO_NAME = "bluer-agent"
 
--- bluer_agent/rag/corpus/build.py
+++ bluer_agent/rag/corpus/build.py
@@ -1,8 +1,13 @@
+from tqdm import tqdm
+
 from blueness import module
 from bluer_options.logger.config import log_list
-from bluer_objects.metadata import get_from_object
+from bluer_objects import objects
+from bluer_objects.file import list_of
+from bluer_objects.file import name as file_name
 
 from bluer_agent import NAME
+from bluer_agent.crawl import file
 from bluer_agent.logger import logger
 
 
@@ -15,9 +20,33 @@ def build(
 ) -> bool:
     logger.info(f"{NAME}.build: {crawl_object_name} -> {corpus_object_name}")
 
-    corpus_roots = get_from_object(crawl_object_name, "corpus", [])
-    log_list(logger, "loaded", corpus_roots, "root(s)")
+    list_of_filenames = list_of(
+        objects.path_of(
+            object_name=crawl_object_name,
+            filename="*.pkl.gz",
+        )
+    )
+    log_list(
+        logger,
+        "processing",
+        [file_name(filename) for filename in list_of_filenames],
+        "file(s)",
+    )
 
-    logger.info("🪄")
+    for filename in tqdm(list_of_filenames):
+        logger.info("processing {} ...".format(file_name(filename)))
+
+        success, crawl = file.load(filename)
+        if not success:
+            return success
+
+        for key, text in tqdm(crawl.items()):
+            logger.info(
+                "processing {}/{} ...".format(
+                    file_name(filename),
+                    key,
+                ),
+            )
+            logger.info("🪄")
 
     return True
