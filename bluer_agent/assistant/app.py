219eb1f24f0d475e44293ce8f12dc91fd01251d1
kamangir <arash@kamangir.net>
2026-02-13 18:27:40 +0330

    wip

--- bluer_agent/assistant/app.py
+++ bluer_agent/assistant/app.py
@@ -16,6 +16,8 @@ from bluer_agent.assistant.endpoints.delete_convo import delete_convo
 from bluer_agent.assistant.endpoints.delete_interaction import delete_interaction
 from bluer_agent.assistant.endpoints.delete_reply import delete_reply
 from bluer_agent.assistant.endpoints.home import home
+from bluer_agent.assistant.endpoints.move_reply_down import move_reply_down
+from bluer_agent.assistant.endpoints.move_reply_up import move_reply_up
 from bluer_agent.assistant.endpoints.next import next
 from bluer_agent.assistant.endpoints.new import new
 from bluer_agent.assistant.endpoints.open import open_conversation
